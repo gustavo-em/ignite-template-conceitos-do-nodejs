c4936340c220150894846a3a4e4fecf0edcd15d1
Gustavo Emanuel Farias Rosa <gustavo.emanuel01@outlook.com>
2022-01-05 03:28:55 -0300

    serverless

--- src/index.js
+++ src/index.js
@@ -8,6 +8,72 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const AWS = require("aws-sdk");
+const serverless = require('serverless-http')
+
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDbClientParams = {};
+if (process.env.IS_OFFLINE) {
+  dynamoDbClientParams.region = 'localhost'
+  dynamoDbClientParams.endpoint = 'http://localhost:3000'
+}
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient(dynamoDbClientParams);
+
+
+app.get("/todos/:userId", async function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      const { userId, todos } = Item;
+      res.json({ userId, todos });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
+
+
+app.post("/todos/:userId", async function (req, res) {
+  const { title, deadline } = req.body;
+  if (typeof title !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } 
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      id: uuidv4(),
+      userId: userId,
+      title,
+      done: false,
+      deadline: new Date(deadline),
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json(Item);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create a todo to user" });
+  }
+});
+
+
+
+
 const users = [];
 
 //check user exists for new request
@@ -116,15 +182,25 @@ app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
 
   const user = users.find((u) => u.username == username);
 
-  const indexTodo = user.todos.findIndex((t) => t.id == id);
+  const todoExists = user.todos.find((t) => t.id == id);
 
-  //console.log(indexTodo);
-  if (indexTodo != -1) {
-    user.todos.splice(indexTodo, 1);
-    return response.status(204).json(user);
+  if (todoExists) {
+    let userTodoRemoved = user.todos.filter((t)=>t.id !== id)
+
+    user.todos = userTodoRemoved
+
+    return response.status(204).json(user.todos);
   } else {
     return response.status(404).json({ error: "Todo Dont Found" });
   }
 });
 
-module.exports = app;
+
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
